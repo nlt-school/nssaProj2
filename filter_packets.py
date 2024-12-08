e22b58891771a77f3f1521d1042d46b56245de3a
Xander-W-Wise <143832636+Xander-W-Wise@users.noreply.github.com>
2024-12-08 11:49:25 -0800

    The modified version that filters the txt
    I changed it because I realized I had to filter the txt not the pcap

--- filter_packets.py
+++ filter_packets.py
@@ -1,31 +1,53 @@
 #!/bin/python3
 
 import scapy.all as scapy
-import subprocess
 
 def filter_packets(value):
 
     pcap = f'Node{value}.pcap'
-    filtered = f'Node{value}_filtered.txt'
-
-    subprocess.run(f'touch {filtered}', shell=True)
+    packetid = set()
+    packets = scapy.rdpcap(pcap)
     
-    packets  = scapy.rdpcap(pcap)
-
-    filteredPackets = []
+    count = 0
     for packet in packets:
+        count+=1
         if packet.haslayer(scapy.ICMP) and packet[scapy.ICMP].type in [0,8]:
-            filteredPackets.append(packet)
+            packetid.add(count)
 
+    return packetid
+
+def locate_and_write_packets(value, packetid):
+ 
+    txtfile = f'Node{value}.txt'
+    filtered = f'Node{value}_filtered.txt'
+    
+    emptycount = 0
+    data = []
+    packet = []
+
+    with open(txtfile) as file:
+        for line in file:
+            if line.strip() == "":
+                emptycount+=1
+                packet.append(line)
+                if emptycount == 2:
+                    data.append(packet)
+                    packet = []
+                    emptycount = 0
+            else:
+                packet.append(line)
 
     with open(filtered, 'w') as file:
-        for packet in filteredPackets:
-            file.write(packet.summary() + '\n')
-
+        for packet in data:
+            packet_number = int(packet[1].strip().split()[0])
+            if packet_number in packetid:
+                for line in packet:
+                    file.write(line)
 def main():
     count = 1
     while count < 5:
-        filter_packets(count)
+        packetid = filter_packets(count)
+        locate_and_write_packets(count, packetid)
         count += 1
 
 if __name__ == '__main__':
